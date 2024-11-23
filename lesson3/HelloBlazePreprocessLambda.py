5bc2392ed5acd4d86bd43daf04b3d8cccb505212
nov05 <215590+Nov05@users.noreply.github.com>
2024-11-22 20:46:25 -0600

    preprocess lambda triggered successfully

--- lesson3/HelloBlazePreprocessLambda.py
+++ lesson3/HelloBlazePreprocessLambda.py
@@ -4,14 +4,18 @@ import os
 import boto3 # type: ignore
 from botocore.exceptions import ClientError # type: ignore
 
-BUCKET_NAME = 's3://sagemaker-studio-807711953667-mmx0am1bt28'
-PREFIX = 'lambda_hands_on'
 
-# Function below unzips the archive to the local directory. 
+## folder to store the transformed data
+BUCKET_NAME = 'sagemaker-studio-807711953667-mmx0am1bt28'
+PREFIX = 'lambda_output'
+
+
+# Function below unzips the archive to the "local" directory. 
 def unzip_data(input_data_path):
     with zipfile.ZipFile(input_data_path, 'r') as input_data_zip:
         input_data_zip.extractall('/tmp/')
         return '/tmp/' + input_data_zip.namelist()[0]
+
 
 # Input data is a file with a single JSON object per line with the following format: 
 # {
@@ -31,7 +35,7 @@ def unzip_data(input_data_path):
 
 
 # We are specifically interested in the fields "helpful" and "reviewText"
-def label_data(input_data):
+def get_labeled_data(input_data):
     labeled_data = []
     HELPFUL_LABEL = "__label__1"
     UNHELPFUL_LABEL = "__label__2"
@@ -61,18 +65,24 @@ def split_sentences(labeled_data):
                 new_split_sentences.append(" ".join([label, s]))
     return new_split_sentences
 
-def upload_data(file_name):
-    object_name = os.path.join(PREFIX, os.path.basename(file_name))
+
+def upload_data(file_path):
+    '''upload data from "local" to S3'''
+    object_name = os.path.join(PREFIX, os.path.basename(file_path))  ## /<prefix>/<file_name>
     s3_client = boto3.client('s3')
     try:
-        response = s3_client.upload_file(file_name, BUCKET_NAME, object_name)
+        response = s3_client.upload_file(file_path, BUCKET_NAME, object_name)
     except ClientError as e:
         logging.error(e)
         return False
 
-def write_data(data, b_name, proportion):
-    train_path = '/tmp/' + b_name + '_train'
-    test_path = '/tmp/' +  b_name + '_test'
+
+def write_data(data, base_name, proportion):
+    '''
+    train-test split, and write them to "local" /tmp, then upload to S3
+    '''
+    train_path = '/tmp/' + base_name + '_train.txt'
+    test_path = '/tmp/' +  base_name + '_test.txt'
     border_index = int(proportion * len(data))
     train_f = open(train_path, 'w')
     test_f = open(test_path, 'w')
@@ -88,7 +98,9 @@ def write_data(data, b_name, proportion):
     upload_data(train_path)
     upload_data(test_path)
 
+
 def download_data(s3_input_uri):
+    '''Get input object, and download it to "local" /tmp'''
     s3 = boto3.client('s3')
     input_bucket = s3_input_uri.split('/')[0]
     input_object = '/'.join(s3_input_uri.split('/')[1:])
@@ -96,10 +108,12 @@ def download_data(s3_input_uri):
     s3.download_file(input_bucket, input_object, file_name)
     return file_name
         
-def preprocess(s3_input_uri):
-    f_name = download_data(s3_input_uri)
-    unzipped_path = unzip_data(f_name)
-    labeled_data = label_data(unzipped_path)
-    new_split_sentence_data = split_sentences(labeled_data)
-    write_data(new_split_sentence_data, os.path.basename(s3_input_uri), .9)
 
+def preprocess(s3_input_uri):
+    fname = download_data(s3_input_uri)
+    unzipped_fpath = unzip_data(fname)
+    labeled_data = get_labeled_data(unzipped_fpath)
+    new_split_sentence_data = split_sentences(labeled_data)
+    write_data(new_split_sentence_data, 
+               os.path.basename(s3_input_uri).split('.')[0], 
+               .9)
